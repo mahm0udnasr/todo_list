f68ee917dfe1e6143c33a25f6cce10ab1570dc28
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-03 18:03:17 +0300

    add middleware and check health server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
-import express from 'express';
+import express from "express";
+import { config } from "dotenv";
+config();
 
 const app = express();
-const PORT  = process.env.PORT;
+const PORT = process.env.PORT;
+
+// middlewares
 app.use(express.json());
 
+// routes
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Server is running" });
+});
+// route for not found routes
+app.use((req, res) => {
+  res.status(404).json({ success: false, message: "route not found" });
+});
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: "Something went wrong" });
+});
 
-
-
-app.listen()
+app.listen(PORT, () => {
+  console.log(`server run on: http://localhost:${PORT}`);
+});
